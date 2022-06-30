fcae3295f4ba35ed89dcfe32a94a5aa5ed3d853b
Carlos Assunção <lucasias85@gmail.com>
2022-06-30 18:07:43 -0300

    #01 Funcional

---
+++ AppCNCRED/AppCNCRED/DAL/TaxaInfo.cs
@@ -0,0 +1,29 @@
+﻿using AppCNCRED.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppCNCRED.DAL
+{
+    public class TaxaInfo
+    {
+        TaxaDTO ValorTaxas(TaxaDTO dto)
+        {
+            dto.ValorTaxa[00] = 0.083700;
+            dto.ValorTaxa[01] = 0.121080;
+            dto.ValorTaxa[02] = 0.155080;
+            dto.ValorTaxa[03] = 0.185447;
+            dto.ValorTaxa[04] = 0.211953;
+            dto.ValorTaxa[05] = 0.234393;
+            dto.ValorTaxa[06] = 0.252607;
+            dto.ValorTaxa[07] = 0.266453;
+            dto.ValorTaxa[08] = 0.275813;
+            dto.ValorTaxa[09] = 0.280620;
+            dto.ValorTaxa[10] = 0.280820;
+            dto.ValorTaxa[11] = 0.276400;
+
+            return dto;
+        }
+    }
+}
--- AppCNCRED/AppCNCRED/MainPage.xaml.cs
+++ AppCNCRED/AppCNCRED/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppCNCRED.DAL;
+using AppCNCRED.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,37 @@ namespace AppCNCRED
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void buttonCalcular_Clicked(object sender, EventArgs e)
+        {
+            double resultado;
+            double valor = Convert.ToDouble(textBoxValor.Text);
+            double[] valorTaxa = new double[12];
+
+            List<String> item = new List<string>();
+            String texto;
+
+            valorTaxa[00] = 0.083700;
+            valorTaxa[01] = 0.121080;
+            valorTaxa[02] = 0.155080;
+            valorTaxa[03] = 0.185447;
+            valorTaxa[04] = 0.211953;
+            valorTaxa[05] = 0.234393;
+            valorTaxa[06] = 0.252607;
+            valorTaxa[07] = 0.266453;
+            valorTaxa[08] = 0.275813;
+            valorTaxa[09] = 0.280620;
+            valorTaxa[10] = 0.280820;
+            valorTaxa[11] = 0.276400;
+
+            for (int i = 0; i < 12; i++)
+            {
+                resultado = (valor * valorTaxa[i]) + valor;
+                texto = Convert.ToString("Parcelamento em " + (i+1) + "X: " + "\n Total a pagar: R$" + resultado);
+                item.Add(texto);
+            }
+            myList.ItemsSource = item;
         }
     }
 }
