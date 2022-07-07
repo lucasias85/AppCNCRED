af8ebde562746ab79b05db68ba8850ab45d2e131
Carlos Assunção <lucasias85@gmail.com>
2022-07-07 18:21:35 -0300

    Versão Atualizada com Taxa Efetiva

--- AppCNCRED/AppCNCRED/MainPage.xaml.cs
+++ AppCNCRED/AppCNCRED/MainPage.xaml.cs
@@ -21,25 +21,27 @@ namespace AppCNCRED
 
         private void buttonCalcular_Clicked(object sender, EventArgs e)
         {
-            item.Clear();
-
-            dto.ValorEmprestimo = Convert.ToDecimal(textBoxValor.Text);
-
-            for (int i = 0; i < 12; i++)
+            if ((textBoxValor.Text == null) || (Convert.ToDecimal(textBoxValor.Text) == 0))
             {
-                dto.NParcela = i + 1;
-                dto.ValorTaxa = dal.PegarTaxa(i);
-                dto.ValorTotal = (dto.ValorEmprestimo * dto.ValorTaxa) + dto.ValorEmprestimo ;
-                dto.ValorParcela = dto.ValorTotal / dto.NParcela;
-                item.Add(new ParcelamentoDTO { ValorEmprestimo = dto.ValorEmprestimo, NParcela = dto.NParcela, ValorTaxa = dto.ValorTaxa, ValorParcela = dto.ValorParcela, ValorTotal = dto.ValorTotal });
+                DisplayAlert("ATENÇÃO!!!", "Verifique a entrada pois o valor é inválido", "OK");
             }
-            myList.ItemsSource = item;
-        }
+            else
+            {
+                item.Clear();
 
-        private void MyList_ItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            var parcela = e.Item as ParcelamentoDTO;
-            DisplayAlert("Mais Informações...", $"Número de Parcela: {parcela.NParcela} \n Valor da Parcela: R$ {parcela.ValorParcela:N} \n Taxa Total de: {(parcela.ValorTaxa) * 100}% \n Total a cobrar: R$ {parcela.ValorTotal:N}", "OK");
+                dto.ValorEmprestimo = Convert.ToDecimal(textBoxValor.Text);
+
+                for (int i = 0; i < 12; i++)
+                {
+                    dto.NParcela = i + 1;
+                    dto.ValorTaxa = dal.PegarTaxa(i);
+                    dto.ValorTotal = (dto.ValorEmprestimo * dto.ValorTaxa) + dto.ValorEmprestimo;
+                    dto.ValorParcela = dto.ValorTotal / dto.NParcela;
+                    dto.ValorTaxa *= 100;
+                    item.Add(new ParcelamentoDTO { ValorEmprestimo = dto.ValorEmprestimo, NParcela = dto.NParcela, ValorTaxa = dto.ValorTaxa, ValorParcela = dto.ValorParcela, ValorTotal = dto.ValorTotal });
+                }
+                myList.ItemsSource = item;
+            }
         }
     }
 }
